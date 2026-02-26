bed75676da13363178764bc029cb52076191a92b
applandsys <applandsys@gmail.com>
2026-02-26 13:22:32 +0600

    Feat: last update

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('products', ProductController::class);
+Route::apiResource('orders', OrderController::class)->except(['update', 'destroy']);
+Route::patch('orders/{id}/status', [OrderController::class,'updateStatus']);
